9d2cf336fbaab89bfc2db9a3dc3fe768909e5e0d
Chandana N <Chandana N@TYSS-Chandana>
2024-06-08 20:11:54 +0530

    i have added a new script inside beauty package(Script01)

---
+++ AdvE8/src/test/java/beauty/Script01.java
@@ -0,0 +1,12 @@
+package beauty;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Script01 {
+
+	@Test
+	public void script1() {
+		Reporter.log("script1",true);
+	}
+}
