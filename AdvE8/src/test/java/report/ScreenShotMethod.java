5697b5706188933479db6cdcc3323f675df65150
Chandana N <Chandana N@TYSS-Chandana>
2024-06-08 19:32:51 +0530

    first commit

---
+++ AdvE8/src/test/java/report/ScreenShotMethod.java
@@ -0,0 +1,30 @@
+package report;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+
+public class ScreenShotMethod {
+
+	public String takeScreenShot(WebDriver driver) {
+		LocalDateTime dateTime= LocalDateTime.now();
+		String date = dateTime.toString().replace(":", "-");
+		TakesScreenshot ts= (TakesScreenshot)driver;
+		String path= "./screenShot/"+date+".png";
+		File source= ts.getScreenshotAs(OutputType.FILE);
+		File target= new File(path);
+		try {
+			FileHandler.copy(source, target);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "." + path;
+	}
+	
+	
+}
